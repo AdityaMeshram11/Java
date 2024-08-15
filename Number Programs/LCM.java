3bb564fcf8d13d9ac380c0a6fa62c63ea1bb4d80
AdityaMeshram11 <adityameshram148@gmail.com>
2024-08-15 20:02:04 +0530

    Update LCM.java

--- Number Programs/LCM.java
+++ Number Programs/LCM.java
@@ -13,12 +13,11 @@ class LCM
 	int n2 = sc.nextInt();
 	int large = (n1>n2) ? n1 : n2 ; // find highest number 
 	int i = 1 ; 
-	//n1 =20 n2= 30  i = 1  large = 30 
 	
 	 while (true) 
 	{
-	 if ((large*1)%n1 == 0 && (large*1) % n2 == 0 ) // 1. >>> 30*1 % 20 == 10 false and 30*1 % 30 == 0  
-													//2.  >>> 30*2 % 20 == 00 treu  and 30*2 % 30 == 0 true
+	 if ((large*1)%n1 == 0 && (large*1) % n2 == 0 ) 
+					
 	 {
 		System.out.println("The LCM of "+n1+" and " +n2+ " is :- " +(large*I)); //execute in iteratin 2
 		break ;
